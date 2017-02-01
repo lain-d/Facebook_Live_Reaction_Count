b0b50718c74d4a9184ba67fe229df09b60bae028
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-02-01 17:15:22 -0500

    chagned for black ink need to use app

--- js/fbvote.js
+++ js/fbvote.js
@@ -151,11 +151,11 @@ function voteArrayCounter(data, next) {
         realtimer[v.type]++;
         if(v.type == "LIKE" && realtimer["LIKE"] > oldlikes)
         {
-            setTimeout(function(){animatevote("like", 300, 600);}, getRandomInt(50, 2500));
+            setTimeout(function(){animatevote("like", 300, 500);}, getRandomInt(50, 2500));
         }
         if(v.type == "LOVE" && realtimer["LOVE"] > oldloves)
         {
-             setTimeout(function(){animatevote("love", 700, 1000);}, getRandomInt(50, 2500));
+             setTimeout(function(){animatevote("love", 700, 900);}, getRandomInt(50, 2500));
         }
     });
     if (next) {
