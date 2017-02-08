ef9def4412c4e18f1c0744b9567950eab0e5c16d
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-02-08 16:15:17 -0500

    updated for blackink vote

--- js/fbvote.js
+++ js/fbvote.js
@@ -6,6 +6,8 @@ var realtimer = { "LIKE": 0, "LOVE": 0, "WOW": 0, "HAHA": 0, "SAD": 0, "ANGRY": 
 var insights = { "LIKE": 0, "LOVE": 0, "WOW": 0, "HAHA": 0, "SAD": 0, "ANGRY": 0 };
 var oldloves = 0;
 var oldlikes = 0;
+var oldwow = 0;
+var oldhaha = 0;
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -150,10 +152,16 @@ function voteArrayCounter(data, next) {
     $.each(data, function(i, v) {
         realtimer[v.type]++;
         if (v.type == "LIKE" && realtimer["LIKE"] > oldlikes) {
-            setTimeout(function() { animatevote("like", 300, 490); }, getRandomInt(50, 2500));
+            setTimeout(function() { animatevote("like", 100, 250); }, getRandomInt(50, 2500));
         }
         if (v.type == "LOVE" && realtimer["LOVE"] > oldloves) {
-            setTimeout(function() { animatevote("love", 775, 950); }, getRandomInt(50, 2500));
+            setTimeout(function() { animatevote("love", 400, 550); }, getRandomInt(50, 2500));
+        }
+                if (v.type == "HAHA" && realtimer["HAHA"] > oldhaha) {
+            setTimeout(function() { animatevote("haha", 700, 850); }, getRandomInt(50, 2500));
+        }
+                if (v.type == "WOW" && realtimer["WOW"] > oldwow) {
+            setTimeout(function() { animatevote("wow", 1000, 1150); }, getRandomInt(50, 2500));
         }
     });
     if (next) {
@@ -178,6 +186,8 @@ function pageLoop(url) {
 function applyVotes() {
     oldlikes = realtimer.LIKE;
     oldloves = realtimer.LOVE;
+        oldhaha = realtimer.HAHA;
+    oldwow = realtimer.WOW;
     $("#liken").text(realtimer.LIKE);
     $("#loven").text(realtimer.LOVE);
     $("#hahan").text(realtimer.HAHA);
