dfcac51201f05369e320bb737c7d7731a85b16e8
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-05-04 13:53:13 -0400

    update

--- hook.php
+++ hook.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = "EAAEt9EOplQYBAF40Ozha2JVnlZAxzt7ZC8x8VWq8BObJDz4WYk1HbsZA4SYPJkM2J7vfZCYB3Bv3e8lZCuUNQR8GsWHyZA5F9UMenB5w2QWqnOx3faPAZA5sWRjT3kp2P89SJRy0dXOxyTr1PlkfvQXDSZCzOxmnkYxZCNAmPEZA5VJwZDZD";
+$access_token = "EAAEt9EOplQYBAG8sK393u2ZAqniFCzDKIw5S49hZAgWBiuFu9VZCHffkD1DZBBVZAxjpSweQV4VcKThEDItgJhTGwnCctTxQgbG8btCkSKm2f6OYF1JsZBZB8w6jfs2dMfvd2NwYAOgiZAvft966qIPhHoa2phxuKosDVt8qaLrZBcQZDZD";
 $verify_token = "lhhbot";
 $hub_verify_token = null;
 
@@ -13,3 +13,4 @@ if ($hub_verify_token === $verify_token) {
     echo $challenge;
 }
 ?>
+
