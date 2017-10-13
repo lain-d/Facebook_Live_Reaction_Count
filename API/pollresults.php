014b118ca78d78abad12b536409241674dea20e1
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-10-13 13:02:19 -0400

    fixes

--- API/pollresults.php
+++ API/pollresults.php
@@ -6,11 +6,11 @@ if (isset($redis))
 $emojis = ["LIKE", "LOVE", "WOW", "HAHA", "SAD", "ANGRY"];
 $results = $redis->mGet($emojis);
 
-echo "<?xml version=\"1.0\"?><results>";
+echo "<?xml version=\"1.0\"?><r>";
 foreach ($emojis  as $key => $value) {
     echo "<".$value.">".$results[$key]."</".$value.">";
 }
-echo "</results>";
+echo "</r>";
 }
 
 ?>
