03e3fd75ef9ff609536a147c4a7a34a7bee79208
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-10-13 12:50:13 -0400

    fixes

--- API/pollresults.php
+++ API/pollresults.php
@@ -4,15 +4,14 @@ header("Content-type: text/xml");
 $redis = new Predis\Client(getenv('REDIS_URL'));
 if (isset($redis))
 {
-$emojis = ["LIKE", "LOVE", "WOW", "HAHA", "SAD", "ANGRY" ];
+$emojis = ["LIKE", "LOVE", "WOW", "HAHA", "SAD", "ANGRY"];
 $results = $redis->mGet($emojis);
 
-$resultXML = "";
+echo "<?xml version=\"1.0\"?><results>";
 foreach ($emojis  as $key => $value) {
-    // $arr[3] will be updated with each value from $arr...
-    $resultXML+= "<".$value.">".$results[$key]."</".$value.">.";
+    echo "<".$value.">".$results[$key]."</".$value.">.";
 }
-echo "<?xml version=\"1.0\"?><results>".$resultXML."</results>";
+echo "</results>";
 }
 
 ?>
