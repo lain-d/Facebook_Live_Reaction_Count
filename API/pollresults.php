cc5671be52e75d8ceaf641bd822b904a1b95db46
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-10-13 16:33:52 -0400

    fixes

--- API/pollresults.php
+++ API/pollresults.php
@@ -8,6 +8,7 @@ $emojis = ["LIKE", "LOVE", "WOW", "HAHA", "SAD", "ANGRY"];
 $results = $redis->mGet($emojis);
 
 echo "<?xml version=\"1.0\"?><r>";
+echo "<choice1>HAHA</choice1><choice2>WOW</choice2>";
 foreach ($emojis  as $key => $value) {
     echo "<".$value.">".$results[$key]."</".$value.">";
 }
